308c10a823cbee1061bae300c58183ebbd7d3c80
cijie.l <cjlee8110@gmail.com>
2017-12-25 14:35:12 +0800

    Rename netstat component

---
+++ netstat/run.js
@@ -0,0 +1,19 @@
+const shell = require('shelljs')
+const os = require('os')
+
+import { read, write } from './source'
+
+const IP = os.networkInterfaces().eth0[0].address
+const WS_SECRET = process.env.WS_SECRET
+
+async function main () {
+  const env = JSON.parse(await read())
+  if (!env.netstatIP) {
+    env.netstatIP = IP
+    await write(env)
+  }
+  shell.cd('/eth-netstats')
+  shell.exec(`WS_SECRET=${WS_SECRET} npm start`)
+}
+
+main()
